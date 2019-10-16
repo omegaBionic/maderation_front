2ea9cd9c6e178b07305004646e68e37b016ae6d4
Guillaume lefebvre <melodia.nocturnis@gmail.com>
2019-10-16 09:43:28 +0200

    test

--- login.cpp
+++ login.cpp
@@ -1,11 +1,13 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QDebug>
 
 Login::Login(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Login)
 {
     ui->setupUi(this);
+    qDebug() << "test";
 }
 
 Login::~Login()
