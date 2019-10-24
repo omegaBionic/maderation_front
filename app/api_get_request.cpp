3b73f083eccb64f24dba8364d7c4b8e66e0b2a27
FERREIRA James <XN5798@engie.com>
2019-10-24 15:55:49 +0200

    Ajout de la class api_get_request avec les requete http : all, user; + parse json file

---
+++ app/api_get_request.cpp
@@ -0,0 +1,6 @@
+#include "api_get_request.h"
+
+api_get_request::api_get_request(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ app/api_get_request.h
@@ -0,0 +1,17 @@
+#ifndef API_GET_REQUEST_H
+#define API_GET_REQUEST_H
+
+#include <QObject>
+
+class api_get_request : public QObject
+{
+    Q_OBJECT
+public:
+    explicit api_get_request(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // API_GET_REQUEST_H
