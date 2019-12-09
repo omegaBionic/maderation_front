67b9e10306babfd09b64b5d6ec670255ffa66ae5
Ferreira <james.71@hotmail.fr>
2019-12-09 21:23:35 +0100

    test class bdd_global

--- app/DB_CLASSES/bdd_global.cpp
+++ app/DB_CLASSES/bdd_global.cpp
@@ -1,6 +1,21 @@
 #include "bdd_global.h"
 
-bdd_global::bdd_global()
+bdd_global::bdd_global(QString id, QString table)
 {
+    _Id = id;
+    _Table = table;
+}
 
+void bdd_global::setId(QString Id){
+    _Id = Id;
+}
+
+QString bdd_global::getId(){
+    return _Id;
+}
+
+QMap<QString, QString> bdd_global::getDict(){
+    QMap<QString, QString> map;
+    map.insert("key", "value");
+    return map;
 }
--- app/DB_CLASSES/bdd_global.h
+++ app/DB_CLASSES/bdd_global.h
@@ -1,11 +1,25 @@
 #ifndef BDD_GLOBAL_H
 #define BDD_GLOBAL_H
-
+#include <QFile>
+#include <QMap>
 
 class bdd_global
 {
 public:
+     explicit bdd_global(QString, QString);
     bdd_global();
+    ~bdd_global();
+
+    void setId(QString);
+    void setTable(QString);
+
+    QString getId();
+    QString getTable();
+    QMap<QString, QString> getDict();
+
+private:
+    QString _Id;
+    QString _Table;
 };
 
 #endif // BDD_GLOBAL_H
