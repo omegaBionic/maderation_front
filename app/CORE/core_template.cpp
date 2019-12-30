689cc738de889139dec7ab485635d08b2d2a18b6
Stark <james.71@hotmail.fr>
2019-12-30 13:52:38 +0100

    modif core_template

--- app/CORE/core_template.cpp
+++ app/CORE/core_template.cpp
@@ -10,12 +10,13 @@ QVector<bdd_PROJECT> core_template::getTemplates(){
 
    api_get_request *get = new api_get_request();
 
-   QVector <bdd_QUOTATION> quot;
+   QVector <bdd_QUOTATION> vQuotation;
+   QVector <bdd_PROJECT> vProject;
 
-   quot = get->parse_file_quotation();
+   vQuotation = get->parse_file_quotation();
 
-   for (int i = 0; i < quot.size(); ++i) {
-       if (quot.indexOf("isTemplate") == true){
+   for (int i = 0; i < vQuotation.size(); ++i) {
+       if (vQuotation.indexOf("isTemplate") == true){
 
        }
 
